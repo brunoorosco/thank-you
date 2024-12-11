fc17fc0b9ea1da93917cd2bc5be1c7f3a564ceb2
Bruno Orosco <bruno.orosco@55pbx.com>
2024-12-11 10:42:34 -0300

    ajustes

--- src/app/layout.js
+++ src/app/layout.js
@@ -25,7 +25,7 @@ export default function RootLayout({ children }) {
       >
         {children}
         <footer class="bg-zinc-50 text-center dark:bg-neutral-700 lg:text-left">
-          <div class="bg-black/5 p-4 text-center text-surface dark:text-white">
+          <div class="bg-black/5 p-2 text-center text-surface dark:text-white">
             © 2024 Copyright:
             <label> Bruno Orosco</label>
           </div>
--- src/app/thank-you/rachel/page.js
+++ src/app/thank-you/rachel/page.js
@@ -10,29 +10,30 @@ const message1 = "Desejo um Feliz Natal e um próspero Ano Novo!";
 export default function ThankYou() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-blue-500 to-purple-600">
-      <div className="mb-1">
-        <motion.div
-          initial={{ opacity: 0, scale: 0.8 }}
-          animate={{ opacity: 1, scale: 1 }}
-          transition={{ duration: 0.8 }}
-          className="overflow-hidden border-4 border-white rounded-full shadow-lg"
-        >
-          <Image
-            width={200}
-            height={200}
-            src={"/rachel.jpg"}
-            alt="LinkedIn Profile"
-            className="object-cover"
-          />
-        </motion.div>
-      </div>
+      <div className="w-full max-w-md mx-auto h-screen flex flex-col items-center justify-center px-4">
+        <div className="mb-1">
+          <motion.div
+            initial={{ opacity: 0, scale: 0.8 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.8 }}
+            className="overflow-hidden border-4 border-white rounded-full shadow-lg"
+          >
+            <Image
+              width={200}
+              height={200}
+              src={"/rachel.jpg"}
+              alt="LinkedIn Profile"
+              className="object-cover"
+            />
+          </motion.div>
+        </div>
 
-      <div className="p-6 text-center text-white">
-        <h1 className="mb-4 text-4xl font-bold">Muito Obrigado</h1>
-        <h1 className="mb-4 text-5xl font-bold">{name}!</h1>
-        <p className="container lg:w-1/3 mx-auto mb-6 text-lg">{message}</p>
-        <p className="container lg:w-1/3 mx-auto text-lg">{message1}</p>
+        <div className="p-6 text-center text-white">
+          <h1 className="mb-4 text-4xl font-bold">Muito Obrigado</h1>
+          <h1 className="mb-4 text-5xl font-bold">{name}!</h1>
+          <p className="mb-6 text-lg">{message}</p>
+          <p className="text-lg">{message1}</p>
+        </div>
       </div>
-    </div>
-  );
+    </div>  );
 }
